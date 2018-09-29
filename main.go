808fb45d9459a87156b09127f75190a106bdbe7b
AmirrezaAsk <raskarpour@gmail.com>
2018-09-29 23:50:15 +0330

    init

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"github.com/amirrezaask/sentinel"
+)
+
+func main() {
+	r := sentinel.New(8080, sentienl.InputTasks{sentinel.InputJSONTask}, sentinel.OutputTasks{sentienl.OutputJSONTask})
+	r.POST("/user/register")
+	r.POST("/user/login")
+	r.POST("/module/add")
+	r.POST("/module/send")
+	r.POST("module/news")
+	r.POST("module/remove")
+	// r.Start()
+}
