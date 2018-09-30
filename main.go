e358658e35c7f0a96efb90f61277a69c76ff3920
AmirrezaAsk <raskarpour@gmail.com>
2018-09-30 19:44:31 +0330

    some user code

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 
 func main() {
 	r := sentinel.New(8080, sentienl.InputTasks{sentinel.InputJSONTask}, sentinel.OutputTasks{sentienl.OutputJSONTask})
+
 	r.POST("/user/register")
 	r.POST("/user/login")
 	r.POST("/module/add")
--- models/user.go
+++ models/user.go
@@ -1,11 +1,48 @@
 package user
 
+import (
+	"github.com/dchest/uniuri"
+	"golang.org/x/crypto/bcrypt"
+)
 
-type User struct{}
+type User struct {
+	UserID      string
+	Name        string
+	LastName    string
+	PhoneNumber string
+	Password    string
+	Email       string
+}
 
+//New will return a New User
+func New(
+	name string,
+	lastName string,
+	phoneNumber string,
+	email string,
+	password_plain string,
+) *User {
+	thisUser := &User{}
+	thisUser.UserID = uniuri.NewLen(12)
+	thisUser.Email = email
+	thisUser.LastName = lastName
+	thisUser.Password = HashPassword(password_plain)
+}
 
-func New(){}
+func Find() error {}
 
-func Find(){}
+func Remove() error {}
 
-func Remove(){}
+func Save() error {}
+
+// helpers
+
+func HashPassword(password string) string {
+	bytes, _ := bcrypt.GenerateFromPassword([]byte(password), 14)
+	return string(bytes)
+}
+
+func CheckPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}
